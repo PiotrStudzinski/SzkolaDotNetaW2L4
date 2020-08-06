f146082f7f11294d9ea3516990bee8d490bd6241
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-06 21:03:05 +0200

    Zakończene zadania 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -6,7 +6,19 @@ namespace Task3
     {
         static void Main(string[] args)
         {
-            
+            double widthRectangle;
+            double lenghtRectangle;
+            double diagonalRectangle;
+
+            Console.Write("Podaj szerokość boku prostokąta: ");
+            widthRectangle = double.Parse(Console.ReadLine());
+            Console.Write("Podaj wysokość boku prostokąta: ");
+            lenghtRectangle = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            diagonalRectangle = Math.Sqrt(Math.Pow(widthRectangle, 2) + Math.Pow(lenghtRectangle, 2));
+            Console.WriteLine($"Przekątna prostokąta dla podanych danych wynosi: {diagonalRectangle}.");
+
         }
     }
 }
