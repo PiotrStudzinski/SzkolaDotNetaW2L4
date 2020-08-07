0f5b0e77df4940b39cad003555fa82758b0452a1
PiotrStudzinski <piotr.studzinski@gmail.com>
2020-08-07 19:46:12 +0200

    Zakończenie zadania 5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -6,7 +6,29 @@ namespace Task5
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Podaj imię: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Podaj nazwisko: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Podaj numer telefonu: ");
+            string phone = Console.ReadLine();
+
+            Console.Write("Podaj adres email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Podaj wiek: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Podaj wzrost: ");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.Write("Podaj datę urodzenia (rrrr-mm-dd): ");
+            DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Podaj płać (\"m\" lub \"k\": ");
+            char sex = char.Parse(Console.ReadLine());
         }
     }
 }
